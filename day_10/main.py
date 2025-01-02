a58dca5ee306eab742dbdf5d42cc22d63707b8dd
sirajm <sirajm@gmail.com>
2025-01-01 20:14:23 -0500

    Day 10 part 2 working and comments added

--- day_10/main.py
+++ day_10/main.py
@@ -12,6 +12,10 @@ TEST_INPUT = """89010123
 
 
 class Vertex:
+	"""
+	A simple class representing a vertex in a graph
+	"""
+
 	def __init__(self, value):
 		self.value = value
 		self.adjacent_vertices = []
@@ -27,13 +31,16 @@ class Vertex:
 
 
 def parse_input(puzzle_input):
-	# Return puzzle input as a 2-d numpy array and a list of all vertices
+	"""
+	Returns 1) puzzle input as a 2-d numpy array and 2) a list of all vertices
+	"""
 	vertices = {}
 	array = puzzle_input.split('\n')
 	array_lists = []
 	for i, line in enumerate(array):
 		array_lists.append(list(line))
 
+		# Add one to row and column because the array ends up padded with a border of -1's at the end
 		for j, element in enumerate(line):
 			vertices[(i + 1, j + 1)] = Vertex(int(element))
 
@@ -41,12 +48,16 @@ def parse_input(puzzle_input):
 
 
 def create_graph(padded_array, _vertices) -> list[Vertex]:
-	"""Creates a graph and returns a list of starting points (Vertices with a value of 0)"""
+	"""
+	Creates a graph representing relationship between all vertices
+	and returns a list of starting points (all vertices with a value of 0)
+	"""
 	starting_vertices = []
 
 	for i, row in enumerate(padded_array):
 		for j, val in enumerate(row):
 
+			# ignore padding
 			if val == -1:
 				continue
 
@@ -67,8 +78,7 @@ def create_graph(padded_array, _vertices) -> list[Vertex]:
 
 	return starting_vertices
 
-
-def traverse_graph(vertex, visited=None):
+def traverse_graph_a(vertex, visited=None):
 	if visited is None:
 		visited = {}
 
@@ -82,21 +92,42 @@ def traverse_graph(vertex, visited=None):
 			continue
 		else:
 			if adjacent.value - vertex.value == 1:
-				traverse_graph(adjacent, visited)
+				traverse_graph_a(adjacent, visited)
+
+def traverse_graph_b(vertex):
+	"""For Part B, counts all paths to a 9;
+	in other words, visiting the same square twice is allowed"""
+	global count
+
+	for adjacent in vertex.adjacent_vertices:
+		if adjacent.value - vertex.value == 1:
+			if adjacent.value == 9:
+				count += 1
+			else:
+				traverse_graph_b(adjacent)
 
 
 with open('input.txt', 'r') as f:
 	REAL_INPUT = f.read()
 
 array, vertices = parse_input(REAL_INPUT)
+
+# pad array with a border of -1's to avoid out-of-bounds error when checking neighbours
 # noinspection PyTypeChecker
 array = np.pad(array, ((1, 1), (1, 1)), constant_values=-1)
+
 starting_points = create_graph(array, vertices)
 
+# Part A
 scores = 0
 for start in starting_points:
-	unique_endings = set()
-	traverse_graph(start)
+	unique_endings = set()  # set to avoid duplicate paths to a single 9
+	traverse_graph_a(start)
 	scores += len(unique_endings)
-
 print(f'Part A: {scores}')
+
+# Part B
+count = 0
+for start in starting_points:
+	traverse_graph_b(start)
+print(f'Part B: {count}')
