8faa9da5043c3394ce23a0d3ab5a77666acc728e
sirajm <sirajm@gmail.com>
2025-01-26 14:42:35 -0500

    day 19 part 1 and 2 working

--- day_19/main.py
+++ day_19/main.py
@@ -20,34 +20,53 @@ def parse_input(puzzle):
 	words = [line for line in puzzle.split('\n\n')[1].split('\n')]
 	return letters, words
 
+
+# REMEMBER TO ADJUST MAX_LEN DEPENDING ON TEST OR REAL INPUT
 @cache
-def is_possible(word, pointer=0, max_len=8):
-	# print(word, pointer)
+def is_possible(word, pointer=0, max_len=8, sequences=0):
 	if pointer >= len(word):
-		return True
+		return 1
 
 	else:
-		for i in range(pointer, pointer + max_len):
-			# print(word[pointer:i + 1])
-			if l.get(word[pointer:i + 1]):
-				if is_possible(word, i + 1):
-					return True
+		if pointer + max_len > len(word):
+			endpoint = len(word)
 
 		else:
-			return False
+			endpoint = pointer + max_len
+
+		for i in range(pointer, endpoint):
+			# print(word[pointer:i + 1])
+			if l.get(word[pointer:i + 1]):
+				sequences += is_possible(word, i + 1)
+
+	return sequences
+
+
+# return sequences
+
+# if valid:
+# 	sequences += 1
+# sequence.append(word[pointer:i + 1])
 
 
 with open('input.txt') as f:
 	REAL_INPUT = f.read()
 
 count = 0
+total_sequences = 0
+
 l, w = parse_input(REAL_INPUT)
 
 for word in w:
-	if is_possible(word):
+	sq = is_possible(word)
+	total_sequences += sq
+	if sq:
 		count += 1
-		print(f'{word} is VALID')
-	else:
-		print(f'{word}')
 
-print(count)
+# print(f'{word} is VALID')
+# print(sequences)
+# else:
+# 	print(f'{word}')
+
+print(f'{count=}')
+print(f'{total_sequences=}')
