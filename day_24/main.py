d62c4dc091a79f09bc149f0553b726e439e5ff02
sirajm <sirajm@gmail.com>
2025-02-08 16:45:21 -0500

    Day 24 part 1 and 2 working

--- day_24/main.py
+++ day_24/main.py
@@ -93,4 +93,72 @@ for instruction in instruct:
 	if instruction.startswith('z'):
 		bits[instruction] = answer(instruction)
 bit_string = ''.join([str(bits[bit]) for bit in sorted(bits)])[::-1]
-print(int(bit_string, 2))
+part_A = int(bit_string, 2)
+print(f'{part_A=}')
+
+"""
+For Part B, key is that the machine is a ripple carry adder,
+consisting with a half adder (which computes least significant bit sum z00 and first carry)
+and a series of full adders afterwards
+
+Credit to this video for a great explanation: https://youtu.be/ecn-8iGDRSo?si=P0p1GDdoTEDtQeYz
+
+Boolean logic needed to determine the pattern:
+
+Half adder: 
+Sum = A XOR B
+Carry_out = A AND B
+
+Full adder:
+Sum = (A XOR B) XOR Carry_in
+Carry_out = (A AND B) OR (Carry_in AND (A XOR B))
+
+Two rules that were used to search the puzzle input for outputs that need to be swapped:
+
+1) Every "z" value (output) has to be a combination of XOR's
+	Three "z" outputs were deemed as culprits because they resulted from combination of two other wires
+	combined with AND, or OR operators
+	
+2) Every output with instructions "x__" AND "y__" has to be combined with an OR operator in the next step
+	Therefore, search all outputs that are the combination of "x__" AND "y__"
+	Then search all instructions for those outputs
+		Identify instructions that do not follow the rule
+	This resulted in 4 instructions that were possible culprits, but the first wrote to z01 which we know is
+	the result of a half adder; so it's ineligible. This narrowed the options down and I checked them all.
+"""
+
+# below code used to figure out pattern #2 above
+
+# all of these outputs should be combined with an OR operator later
+# register_ands = []
+#
+# for output, instructions in instruct.items():
+# 	if instructions[1] == 'AND' and 'x' in [instructions[0][0], instructions[2][0]] and 'y' in [instructions[0][0],
+# 																								instructions[2][0]]:
+# 		register_ands.append(output)
+#
+# pprint(register_ands)
+#
+# for output, instructions in instruct.items():
+# 	if instructions[0] in register_ands or instructions[2] in register_ands:
+# 		if instructions[1] != 'OR':
+# 			print(output, instructions)
+
+# this narrowed down an incorrect output to hfm or fgt; and eventually to fgt
+
+# print('hfm' in register_ands)  # culprit
+# print('hqt' in register_ands)
+# print('fgt' in register_ands)  # culprit
+# print('nmq' in register_ands)
+
+part_B = ['z24',
+		  'fpq',
+		  'z32',
+		  'srn',
+		  'z07',
+		  'nqk',
+		  'fgt',
+		  'pcp']
+
+part_B.sort()
+print('part_B='+','.join(part_B))
